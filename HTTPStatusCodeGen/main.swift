953507aacf4817d013c40d98f2ee7f42cd7eb238
Yuki Nagai <ynagai@r.recruit.co.jp>
2016-07-10 22:11:53 +0900

    Initial commit

---
+++ HTTPStatusCodeGen/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  HTTPStatusCodeGen
+//
+//  Created by Yuki Nagai on 7/10/16.
+//  Copyright © 2016 Recruit Lifestyle Co., Ltd. All rights reserved.
+//
+
+import Foundation
+
+print("DEBUG")
